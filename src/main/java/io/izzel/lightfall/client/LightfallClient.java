d5f8196f11e29669304879bbca80c0663d5d0e2d
IzzelAliz <csh2001331@126.com>
2021-03-08 17:52:50 +0800

    Remove unused code

--- src/main/java/io/izzel/lightfall/client/LightfallClient.java
+++ src/main/java/io/izzel/lightfall/client/LightfallClient.java
@@ -1,28 +1,7 @@
 package io.izzel.lightfall.client;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Mod("lightfallclient")
 public class LightfallClient {
-
-    public static final Queue<Runnable> workQueue = new ConcurrentLinkedQueue<>();
-
-    public LightfallClient() {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void onPreTick(TickEvent.ClientTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            while (!workQueue.isEmpty()) {
-                workQueue.poll().run();
-            }
-        }
-    }
 }
