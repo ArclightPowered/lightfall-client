1d4473d7786892f242ac4697b8d07b81a10a86e7
IzzelAliz <csh2001331@126.com>
2021-03-10 15:04:19 +0800

    Add a button for disconnecting handshake

--- src/main/java/io/izzel/lightfall/client/gui/LightfallHandshakeScreen.java
+++ src/main/java/io/izzel/lightfall/client/gui/LightfallHandshakeScreen.java
@@ -1,10 +1,13 @@
 package io.izzel.lightfall.client.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.DialogTexts;
 import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraft.client.gui.screen.MultiplayerScreen;
 import net.minecraft.client.gui.screen.WorkingScreen;
+import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.network.NetworkManager;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class LightfallHandshakeScreen extends WorkingScreen {
 
@@ -24,5 +27,18 @@ public class LightfallHandshakeScreen extends WorkingScreen {
                 Minecraft.getInstance().displayGuiScreen(new MultiplayerScreen(new MainMenuScreen()));
             }
         }
+    }
+
+    @Override
+    protected void init() {
+        this.displaySavingString(new TranslationTextComponent("connect.connecting"));
+        this.addButton(
+            new Button(this.width / 2 - 100, this.height / 4 + 120 + 12, 200, 20, DialogTexts.GUI_CANCEL,
+                button -> {
+                    this.networkManager.closeChannel(new TranslationTextComponent("connect.aborted"));
+                    this.minecraft.displayGuiScreen(new MultiplayerScreen(new MainMenuScreen()));
+                }
+            )
+        );
     }
 }
