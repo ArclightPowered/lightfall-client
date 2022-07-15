74c5b76deb6e6e184ffa3beaf5ac8de3d3a9622a
IzzelAliz <csh2001331@126.com>
2022-07-15 23:14:13 +0800

    Reset state when disconnected during handshaking

--- src/main/java/io/izzel/lightfall/client/gui/LightfallHandshakeScreen.java
+++ src/main/java/io/izzel/lightfall/client/gui/LightfallHandshakeScreen.java
@@ -40,7 +40,9 @@ public class LightfallHandshakeScreen extends Screen {
         this.addRenderableWidget(
             new Button(this.width / 2 - 100, this.height / 4 + 120 + 12, 200, 20, CommonComponents.GUI_CANCEL,
                 button -> {
-                    this.networkManager.disconnect(new TranslatableComponent("connect.aborted"));
+                    if (this.networkManager.isConnected()) {
+                        this.networkManager.disconnect(new TranslatableComponent("connect.aborted"));
+                    }
                     this.minecraft.setScreen(new JoinMultiplayerScreen(new TitleScreen()));
                 }
             )
--- src/main/java/io/izzel/lightfall/client/mixin/ClientLoginNetHandlerMixin.java
+++ src/main/java/io/izzel/lightfall/client/mixin/ClientLoginNetHandlerMixin.java
@@ -5,11 +5,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientHandshakePacketListenerImpl;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.network.PacketListener;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.registries.GameData;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientHandshakePacketListenerImpl.class)
 public class ClientLoginNetHandlerMixin implements ClientLoginNetHandlerBridge {
@@ -29,5 +33,15 @@ public class ClientLoginNetHandlerMixin implements ClientLoginNetHandlerBridge {
             this.minecraft.level = lightfall$reuse.getLevel();
         }
         return lightfall$reuse == null ? origin : lightfall$reuse;
+    }
+
+    @Inject(method = "onDisconnect", at = @At("HEAD"))
+    private void lightfall$resetState(Component p_104543_, CallbackInfo ci) {
+        if (lightfall$reuse != null && lightfall$reuse.getLevel() != null) {
+            this.minecraft.level = lightfall$reuse.getLevel();
+            this.minecraft.clearLevel();
+        } else {
+            GameData.revertToFrozen();
+        }
     }
 }
